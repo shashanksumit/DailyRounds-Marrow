e20d0a537aa3ef7f280abbe23e3cc29eeae6a633
shashanksumit <shashanksingh485@gmail.com>
2025-05-07 18:36:45 +0530

    Initial commit

---
+++ Focus_Productivity/Focus_ProductivityUITests/Focus_ProductivityUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Focus_ProductivityUITestsLaunchTests.swift
+//  Focus_ProductivityUITests
+//
+//  Created by Shashank Singh on 07/05/25.
+//
+
+import XCTest
+
+final class Focus_ProductivityUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
